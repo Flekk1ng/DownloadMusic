468595ef84cdb934291056d90c3b3bdda1fcba4c
Flekk1ng <39874614+Flekk1ng@users.noreply.github.com>
2021-01-07 23:41:22 +0300

    nc

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.stencilview import StencilView
 from kivy.uix.textinput import TextInput
 
-'''
+
 class Container(GridLayout):
 
     def __init__(self, **kwargs):
@@ -35,52 +35,51 @@ class Container(GridLayout):
                     break
                 lab = Button(text=el['title'])
                 self.add_widget(lab)
-        except:
-            print(Exception.__class__)
+        except Exception:
+            raise
 
-class ScrollView(GridLayout):
+
+class ScrollVw(GridLayout):
 
     def __init__(self, **kwargs):
-        super(ScrollView, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         layout = GridLayout(cols=1, spacing=10, padding=10, size_hint_y=None)
         # Make sure the height is such that there is something to scroll.
         layout.bind(minimum_height=layout.setter('height'))
         try:
             with open("data_audios.json", "r") as rf:
                 data = json.load(rf)
-        except:
-            print(Exception.__class__)
+        except Exception:
+            raise
         for i in data:
             btn = Button(text=i['title'], size_hint_y=None, height=40)
             layout.add_widget(btn)
         self.add_widget(layout)
-'''
 
 
-class Scroll(ScrollView):
+class ScrollT(ScrollView):
     def __init__(self, **kwargs):
-        super(Scroll, self).__init__(**kwargs)
-        layout = GridLayout(cols=1, size_hint_y=0.47, spacing=10)
+        super().__init__(**kwargs)
 
-        layout.bind(minimum_height=layout.setter('height'))  # checks when window maximized
+        self.layout = GridLayout()
+
+        self.layout.cols = 1
+        self.layout.size_hint_y = 0.47
+        self.layout.spacing = 10
+
+        self.layout.bind(minimum_height=self.layout.setter('height'))  # checks when window maximized
 
         for i in range(1, 5):
-            submit = Button(text='something', size_hint_y=None, height=40)
-            layout.add_widget(submit)
+            self.submit = Button(text='something', size_hint_y=None, height=40)
+            self.layout.add_widget(self.submit)
 
-        but = Button(text='Create new window!', size_hint_y=None, height=40)
-        layout.add_widget(but)
-        layout.add_widget(TextInput(text=''))
-        self.add_widget(layout)
-
-    def getscreen(self):
-        return self.screen
+        self.add_widget(self.layout)
 
 
 class Scroll2:
     def __init__(self):
         layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-        # Make sure the height is such that there is something to scroll.
+
         layout.bind(minimum_height=layout.setter('height'))
         for i in range(100):
             btn = Button(text=str(i), size_hint_y=None, height=40)
@@ -93,7 +92,7 @@ class Scroll2:
 
 class MyApp(App):
     def build(self):
-        return
+        return ScrollT()
 
 
 if __name__ == '__main__':
